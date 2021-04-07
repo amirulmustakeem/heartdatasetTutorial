bf1736366d09f0f4e6cc4f94b431ca9b7eb701af
amirul mustakeem <mohdamirulmustakeem@gmail.com>
2021-04-07 16:40:03 +0800

    edit source code

--- sourcecode.R
+++ sourcecode.R
@@ -1,4 +1,15 @@
 #read data
 data<-read.csv("heart.csv")
+
 head(data)
+
+# bar chart for age distribution
+
+data$sex<-factor(data$sex) 
+dataSex=factor(data$sex,labels=c("female","male")) #description of gender
+
+counts<-table(dataSex,data$ï..age)
+
+barplot(counts, main="age distribution", xlab="Age", ylab="count",legend=rownames(counts),beside=TRUE)
+
 
